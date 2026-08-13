6fb4438c30e77a50f50d5f09cd1c696860a28320
Rewan Ahmed <rewan7378@gmail.com>
2026-08-14 02:15:40 +0300

    fix router import

--- src/pages/SavedNews.jsx
+++ src/pages/SavedNews.jsx
@@ -1,6 +1,6 @@
 import "../NewsPage.css";
 import { useEffect, useState } from "react";
-import {link} from "React-router-dom";
+import {link} from "react-router-dom";
 
 function SavedNews() {
   const [savedNews, setSavedNews] = useState([]);
